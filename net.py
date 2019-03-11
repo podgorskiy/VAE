395d8d6d3afc1c6bf9724dcf07b8253be08776eb
Podgorskiy <stanislav@podgorskiy.com>
2019-03-10 22:18:50 -0400

    -

--- net.py
+++ net.py
@@ -15,7 +15,122 @@
 
 import torch
 from torch import nn
+from torch.nn import init
 from torch.nn import functional as F
+from torch.nn.parameter import Parameter
+import numpy as np
+
+
+def is_sequence(arg):
+    return (not hasattr(arg, "strip") and
+            hasattr(arg, "__getitem__") or
+            hasattr(arg, "__iter__"))
+
+
+def make_tuple(x, n):
+    if is_sequence(x):
+        return x
+    return tuple([x for _ in range(n)])
+
+
+def pixel_norm(x, epsilon=1e-8):
+    return x * torch.rsqrt(torch.mean(x**2, dim=1, keepdim=True) + epsilon)
+
+
+class Linear(nn.Module):
+    def __init__(self, in_features, out_features, bias=True, gain=np.sqrt(2.0 / (1 + 0.2 ** 2))):
+        super(Linear, self).__init__()
+        self.in_features = in_features
+        self.weight = Parameter(torch.Tensor(out_features, in_features))
+        if bias:
+            self.bias = Parameter(torch.Tensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.std = 0
+        self.gain = gain
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.std = self.gain / np.sqrt(self.in_features)
+        init.normal_(self.weight)#, std=self.std)
+        if self.bias is not None:
+            with torch.no_grad():
+                self.bias.zero_()
+
+    def forward(self, input):
+        return F.linear(input, self.weight * self.std, self.bias)
+
+
+class Conv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, output_padding=0, dilation=1,
+                 groups=1, bias=True, gain=np.sqrt(2.0 / (1 + 0.2 ** 2)), transpose=False):
+        super(Conv2d, self).__init__()
+        if in_channels % groups != 0:
+            raise ValueError('in_channels must be divisible by groups')
+        if out_channels % groups != 0:
+            raise ValueError('out_channels must be divisible by groups')
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = make_tuple(kernel_size, 2)
+        self.stride = make_tuple(stride, 2)
+        self.padding = make_tuple(padding, 2)
+        self.output_padding = make_tuple(output_padding, 2)
+        self.dilation = make_tuple(dilation, 2)
+        self.groups = groups
+        self.gain = gain
+        self.transpose = transpose
+        if transpose:
+            self.weight = Parameter(torch.Tensor(in_channels, out_channels // groups, *self.kernel_size))
+        else:
+            self.weight = Parameter(torch.Tensor(out_channels, in_channels // groups, *self.kernel_size))
+        if bias:
+            self.bias = Parameter(torch.Tensor(out_channels))
+        else:
+            self.register_parameter('bias', None)
+        self.std = 0
+        self.fan_in = np.prod(kernel_size) * in_channels
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.std = self.gain / np.sqrt(self.fan_in)
+        init.normal_(self.weight)#, std=self.std)
+        if self.bias is not None:
+            with torch.no_grad():
+                self.bias.zero_()
+
+    def forward(self, x):
+        if self.transpose:
+            return F.conv_transpose2d(x, self.weight * self.std, self.bias, stride=self.stride, padding=self.padding,
+                                      output_padding=self.output_padding, dilation=self.dilation, groups=self.groups)
+        else:
+            return F.conv2d(x, self.weight * self.std, self.bias, stride=self.stride, padding=self.padding,
+                            dilation=self.dilation, groups=self.groups)
+
+
+class ConvTranspose2d(Conv2d):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, output_padding=0, dilation=1,
+                 groups=1, bias=True, gain=np.sqrt(2.0 / (1 + 0.2 ** 2))):
+        super(ConvTranspose2d, self).__init__(in_channels, out_channels, kernel_size, stride, padding,
+                                              output_padding, dilation, groups, bias, gain, True)
+
+
+class SeparableConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, output_padding=0, dilation=1,
+                 bias=True, gain=np.sqrt(2.0 / (1 + 0.2 ** 2)), transpose=False):
+        super(SeparableConv2d, self).__init__()
+        self.spatial_conv = Conv2d(in_channels, in_channels, kernel_size, stride, padding, output_padding, dilation,
+                                   in_channels, False, 1, transpose)
+        self.channel_conv = Conv2d(in_channels, out_channels, 1, bias, 1, gain=gain)
+
+    def forward(self, x):
+        return self.channel_conv(self.spatial_conv(x))
+
+
+class SeparableConvTranspose2d(Conv2d):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, output_padding=0, dilation=1,
+                 bias=True, gain=np.sqrt(2.0 / (1 + 0.2 ** 2))):
+        super(SeparableConvTranspose2d, self).__init__(in_channels, out_channels, kernel_size, stride, padding,
+                                              output_padding, dilation, bias, gain, True)
 
 
 class VAE(nn.Module):
@@ -31,8 +146,7 @@ class VAE(nn.Module):
         mul = 1
         inputs = channels
         for i in range(self.layer_count):
-            setattr(self, "conv%d" % (i + 1), nn.Conv2d(inputs, inputs, 4, 2, 1, groups=inputs))
-            setattr(self, "conv%d_1x1" % (i + 1), nn.Conv2d(inputs, d * mul, 1, 1, 0))
+            setattr(self, "conv%d" % (i + 1), nn.Conv2d(inputs, d * mul, 4, 2, 1))
             setattr(self, "conv%d_bn" % (i + 1), nn.BatchNorm2d(d * mul))
             inputs = d * mul
             mul *= 2
@@ -47,18 +161,16 @@ class VAE(nn.Module):
         mul = inputs // d // 2
 
         for i in range(1, self.layer_count):
-            setattr(self, "deconv%d" % (i + 1), nn.ConvTranspose2d(inputs, inputs, 4, 2, 1, groups=inputs))
-            setattr(self, "deconv%d_1x1" % (i + 1), nn.Conv2d(inputs, d * mul, 1, 1, 0))
+            setattr(self, "deconv%d" % (i + 1), nn.ConvTranspose2d(inputs, d * mul, 4, 2, 1))
             setattr(self, "deconv%d_bn" % (i + 1), nn.BatchNorm2d(d * mul))
             inputs = d * mul
             mul //= 2
 
-        setattr(self, "deconv%d" % (self.layer_count + 1), nn.ConvTranspose2d(inputs, inputs, 4, 2, 1, groups=inputs))
-        setattr(self, "deconv%d_1x1" % (self.layer_count + 1), nn.Conv2d(inputs, channels, 1, 1, 0))
+        setattr(self, "deconv%d" % (self.layer_count + 1), nn.ConvTranspose2d(inputs, channels, 4, 2, 1))
 
     def encode(self, x):
         for i in range(self.layer_count):
-            x = F.relu(getattr(self, "conv%d_bn" % (i + 1))(getattr(self, "conv%d_1x1" % (i + 1))(getattr(self, "conv%d" % (i + 1))(x))))
+            x = F.relu(getattr(self, "conv%d_bn" % (i + 1))(getattr(self, "conv%d" % (i + 1))(x)))
 
         x = x.view(x.shape[0], self.d_max * 4 * 4)
         h1 = self.fc1(x)
@@ -81,9 +193,9 @@ class VAE(nn.Module):
         x = F.leaky_relu(x, 0.2)
 
         for i in range(1, self.layer_count):
-            x = F.leaky_relu(getattr(self, "deconv%d_bn" % (i + 1))(getattr(self, "deconv%d_1x1" % (i + 1))(getattr(self, "deconv%d" % (i + 1))(x))), 0.2)
+            x = F.leaky_relu(getattr(self, "deconv%d_bn" % (i + 1))(getattr(self, "deconv%d" % (i + 1))(x)), 0.2)
 
-        x = F.tanh(getattr(self, "deconv%d_1x1" % (self.layer_count + 1))(getattr(self, "deconv%d" % (self.layer_count + 1))(x)))
+        x = ((getattr(self, "deconv%d" % (self.layer_count + 1))(x)))
         return x
 
     def forward(self, x):
